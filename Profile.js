20768e5c79fd4a80083cedfbcee397ed3b95266f
Drasangna <72321239+Drasangna@users.noreply.github.com>
2021-10-11 21:36:38 -0400

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} 
+        onPress = {()=>this.signInWithGoogleAsync()}>
+          <image source={require("../assets/googleicon.png")} style={styles.googleIcon}/> 
+        </TouchableOpacity>
+        <Text>Profile</Text>
+      </View>
+    );
+  }
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
